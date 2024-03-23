97f0a370c22e2a9cb08fe37e6d28b6a0f3b2a9be
Osamakurkur <157221635+Osamakurkur@users.noreply.github.com>
2024-03-24 01:20:15 +0500

    Add files via upload

---
+++ Assignment#17/app.js
@@ -0,0 +1,56 @@
+//QUESTION # 02
+// for(var i = 1 ; i<=10 ;i++)
+// console.log(i)
+
+// var arr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ]
+// console.log(arr)
+
+//QUESTION # 03
+// for(var i = 1 ; i<=10 ;i++)
+// console.log(i)
+
+//QUESTION # 04
+// var userinput= +prompt("enter your number")
+
+// for (var i = 1; i <= 15; i++) {
+//     document.write(userinput+ "x" + i + "=" + i * userinput + "<br /> ")
+
+// }
+
+//QUESTION # 05
+// var arr = ["apple", "banana", "mango", "orange","strawberry"]
+// console.log(arr)
+
+//QUESTION # 06
+// var a = 1
+// for (a ; a<=15; a++)
+// console.log(a)
+
+// var b = 10
+// for (b ; b>=1; b--)
+// console.log(b)
+
+// for (var c = 0; c <= 20; c++) {
+//         if (c % 2 == 0) {
+//             console.log(c)
+//         }
+//     }
+
+// for (var d = 1; d <= 20; d++) {
+//         if (d % 2 !== 0) {
+//             console.log(d)
+//         }
+// //     }
+
+// for (var e = 2; e <= 20; e++) {
+//             if (e % 2 == 0) {
+//                 console.log(e+ "k")
+//             }
+//         }
+
+
+
